25ee8e9a831404e2601041e22449d504d8491afd
Eric Budd <ericbudd@gmail.com>
2014-12-21 13:19:28 -0700

    Add functions makeCacheMatrix() and cacheSolve() to input a matrix, solve for the inverse matrix, and use a cached copy if previously solved.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Input and store an array or matrix
 
-## Write a short comment describing this function
+## 'set' allows for manually setting a matrix
+## 'get' returns a matrix stored in the object
+## 'setinv' allows to manually set the inverse matrix
+## 'getinv' returns the stored inverse matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric(),mat = numeric()) {
+    #set inverse to null since it has not been calculated
+    inv <- NULL
+    
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() {
+        #define matrix based on input parameters
+        mat <<- matrix(x,nrow=sqrt(length(x)),ncol=sqrt(length(x)))
+        
+        #return matrix
+        mat
+    }
+    setinv <- function(solve) inv <<- solve
+    getinv <- function() inv
+    
+    #define public functions
+    list(set = set, get = get,
+    setinv = setinv,
+    getinv = getinv
+    )
 }
 
+## Return a matrix that is the inverse of 'x'
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    inv <- x$getinv()
+    
+    #check if matrix calculation has been performed; if so, return cached data
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    
+    #read input matrix
+    data <- x$get()
+    
+    #calculate inverse matrix
+    inv <- solve(data, ...)
+    
+    #set/return calculated inverse matrix
+    x$setinv(inv)
+    inv
 }
